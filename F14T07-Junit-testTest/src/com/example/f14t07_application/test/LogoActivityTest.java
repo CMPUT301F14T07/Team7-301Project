b7917df38a5f9c3362a9984645bd9ca1341d3902
bbruner <bbruner@ualberta.ca>
2014-10-03 14:53:05 -0600

    Empty project created for the application. Junit test project created with empty test class for the main activity

---
+++ F14T07-Junit-testTest/src/com/example/f14t07_application/test/LogoActivityTest.java
@@ -0,0 +1,14 @@
+package com.example.f14t07_application.test;
+
+import com.example.f14t07_application.LogoActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class LogoActivityTest extends ActivityInstrumentationTestCase2<LogoActivity> {
+
+	public LogoActivityTest( 	) 
+	{
+		super(LogoActivity.class);
+	}
+
+}
