2a2123b5b4404b2be771b39f6bbde1277ab7e7f9
Brendan Bruner <bbruner@ualberta.ca>
2014-10-05 12:54:09 -0600

    Test class for up/down/reply

---
+++ F14T07-Junit-testTest/src/com/example/f14t07_application/test/UpDownReplyTest.java
@@ -0,0 +1,7 @@
+package com.example.f14t07_application.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class UpDownReplyTest extends ActivityInstrumentationTestCase2<T> {
+
+}
