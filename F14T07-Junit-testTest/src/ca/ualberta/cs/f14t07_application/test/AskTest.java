16acab6aa401de12a3e10715ca5c41c4f80222ad
jfryan7 <jfryan@ualberta.ca>
2014-10-07 23:12:03 -0600

    Added class and test class for Ask

---
+++ F14T07-Junit-testTest/src/ca/ualberta/cs/f14t07_application/test/AskTest.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cs.f14t07_application.test;
+
+import ca.ualberta.cs.f14t07_application.Ask;
+import ca.ualberta.cs.f14t07_application.LogoActivity;
+import android.test.ActivityInstrumentationTestCase2;
+import ca.ualberta.cs.f14t07_application.Ask;;
+
+
+public class AskTest extends ActivityInstrumentationTestCase2 {
+	
+	public AskTest()
+	{
+		super(Ask.class);
+	}
+	
+	//Test if there is a name
+	public void testName()
+	{
+		
+	}
+
+}
