61a088e1c697abd691fa9a2060ea6b0dc4ce6897
jfryan7 <jfryan@ualberta.ca>
2014-10-15 12:45:07 -0600

    Updated Ask tests

--- F14T07-Junit-testTest/src/ca/ualberta/cs/f14t07_application/test/AskTest.java
+++ F14T07-Junit-testTest/src/ca/ualberta/cs/f14t07_application/test/AskTest.java
@@ -2,28 +2,37 @@ package ca.ualberta.cs.f14t07_application.test;
 
 import ca.ualberta.cs.f14t07_application.Ask;
 import ca.ualberta.cs.f14t07_application.LogoActivity;
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
-import ca.ualberta.cs.f14t07_application.Ask;;
 
 
 public class AskTest extends ActivityInstrumentationTestCase2<AskActivity> {
 	
-	private Button testButton;
-	
+	private Button testPostButton;
+	private Button testMainMenuButton;
+		
 	public AskTest()
 	{
 		super(AskActivity.class);
 	}
 	
-	/* The current functionality after clicking the "ask" button from the main menu 
-	 * is as follows:
-	 * enter a name
-	 * enter a question
-	 * enter a subject
-	 * attach a picture
-	 * click an "ask" or "submit" button
-	 * click on a "main menu" button
+	@Override
+	protected void setUp() throws Exception
+	{
+	super.setUp();
+	setActivityInitialTouchMode(true);
+	testActivity = getActivity();
+	}
+	
+	
+	/* The current functionality after clicking the "ask" button from the main menu is as follows:
+	 * -enter a name
+	 * -enter a question
+	 * -enter a subject
+	 * -attach a picture
+	 * -click an "ask" or "submit" button
+	 * -click on a "main menu" button
 	 */
 	
 	//Test if there is a name
@@ -32,26 +41,38 @@ public class AskTest extends ActivityInstrumentationTestCase2<AskActivity> {
 		//Will we be saving user names? So should we check if the name exists?
 		//Will users have to sign in?
 		
-		// Need to replace "something" with however we will be creating user names
-		String name = new String("Testname");
-		//name = something.getName();
-		assert.assertNotNull("There is a user name object", name);
-		
-		//**Is there a better way to test that it is not only not null but also acutally a string?
+		boolean notEmpty = false;
+		nameString = (String) testActivity.findViewById(
+				com.example.f14t07_application.activity_testactivity.R.id.NameString);
+		if(!nameString.isEmpty() && nameString.trim().length() > 0)
+		{
+			notEmpty = true;
+		}
+		assertTrue("The name string is empty", notEmpty == true);
 	}
 	
 	public void testQuestion()
 	{
-		String question = new String("test question");
-		//question = something.getQuestion();
-		assert.assertNotNull("There is a question object", question);
+		boolean notEmpty = false;
+		questionString = (String) testActivity.findViewById(
+				com.example.f14t07_application.activity_testactivity.R.id.QuestionString);
+		if(!questionString.isEmpty() && questionString.trim().length() > 0)
+		{
+			notEmpty = true;
+		}
+		assertTrue("The question string is empty", notEmpty == true);
 	}
 	
 	public void testSubject()
 	{
-		String subject = new String("test subject");
-		//subject = something.getSubject();
-		assert.assertNotNull("There is a subject object", subject);
+		boolean notEmpty = false;
+		subjectString = (String) testActivity.findViewById(
+				com.example.f14t07_application.activity_testactivity.R.id.SubjectString);
+		if(!subjectString.isEmpty() && subjectString.trim().length() > 0)
+		{
+			notEmpty = true;
+		}
+		assertTrue("The subject string is empty", notEmpty == true);
 	}
 
 	public void testPicture()
@@ -59,24 +80,39 @@ public class AskTest extends ActivityInstrumentationTestCase2<AskActivity> {
 		//Need more tests in here once we determine more about how pictures will work
 		
 		//Possible a boolean value in the questions attributes indicating if there is a picture?
+
+		boolean isPicture = false;
 		
-		if( ...something.question.picture() == true)
-		{
-			Object picture = ...picture; //Need to figure out how we will deal with pictures
+
+
+		
+		
+		
+		
 			//Check if the pictures are the same (it was loaded properly)
 			
 			//Maybe check picture size?
 		}
 	}
 	
-	public void testAskButton()
+	public void testPostButton()
 	{
-		
+		testPostButton = (Button) AskActivity.findViewById(
+				com.example.f14t07_application.activity_askactivity.R.id.PostButton);	
+		testPostButton.performClick();
+		Intent newintent = getStartedActivityIntent();
+		Intent AskIntent= new Intent(AskTest.this, AskActivity.class); //where should this go? Some view activity?
+		assertTrue(newintent.filterEquals(AskIntent));
 	}
 	
 	public void testMainMenuButton()
 	{
-		
+		testMainMenuButton = (Button) AskActivity.findViewById(
+				com.example.f14t07_application.activity_askactivity.R.id.MainMenuButton);	
+		testMainMenuButton.performClick();
+		Intent newintent = getStartedActivityIntent();
+		Intent AskIntent= new Intent(AskTest.this, HomeActivity.class); 
+		assertTrue(newintent.filterEquals(AskIntent));
 	}
 
 }
