f6dd65be39d42a8f5baa458fffabcddb53786e4b
Brendan Bruner <bbruner@ualberta.ca>
2014-10-17 22:41:57 -0600

    Updated SortText.java to reflect changes in UML

--- F14T07-Junit-testTest/src/ca/ualberta/cs/f14t07_application/test/SortTest.java
+++ F14T07-Junit-testTest/src/ca/ualberta/cs/f14t07_application/test/SortTest.java
@@ -7,12 +7,15 @@ import ca.ualberta.cs.f14t07_application.Question;
 
 import android.test.ActivityInstrumentationTestCase2;
 
-
+/** 
+ * This class tests the different sorting algorithms. These tests are
+ * done by creating a few forum entries, saving them, instantiating
+ * a browse controller, and calling its sorting functions. Finally
+ * the forum entries are loaded from memory and compared to the order
+ * they ought to appear in.
+ * @author Marcin, Lexie, Brendan
+ */
 public class SortTest extends ActivityInstrumentationTestCase2<BrowseActivity> {
-	/* TODO: The Class generic that goes with ActivityInstrumentationTestCase2 is
-	 * an activity class - the activity which will be active when the test cases
-	 * are called.
-	 */
 	private BrowseActivity testActivity;
 	
 	@Override
@@ -39,11 +42,12 @@ public class SortTest extends ActivityInstrumentationTestCase2<BrowseActivity> {
 	}
 	
 	public void sortByTimeTest(){
-		long time=123456789;
 		ArrayList<ForumEntry> posts = new ArrayList<ForumEntry>();
-		posts.add(new ForumEntry("test 1","author1",new Date(time)));
-		posts.add(new ForumEntry("test 2","author2",new Date(time)));
-		posts.add(new ForumEntry("test 3","author3",new Date(time)));
+		posts.add(new ForumEntry("test 1","author1"));
+		Thread.sleep(1000);
+		posts.add(new ForumEntry("test 2","author2"));
+		Thread.sleep(1000);
+		posts.add(new ForumEntry("test 3","author3"));
 		(new DataManager()).save(posts);
 		
 		//set time using index, day month year hour minute
@@ -53,12 +57,12 @@ public class SortTest extends ActivityInstrumentationTestCase2<BrowseActivity> {
 		
 		(new DataManager()).save(posts);
 		
-		testActivity.getBrowseControllerForTesting().sortByTime();
+		(new BrowseController()).sortByTime();
 		
 		ForumEntryList testList= new ForumEntryList();
-		testList.add(new ForumEntry("test 3","author3",new Date(time)));
-		testList.add(new ForumEntry("test 2","author2",new Date(time+2)));
-		testList.add(new ForumEntry("test 1","author1",new Date(time+3)));
+		testList.add(new ForumEntry("test 3","author3"));
+		testList.add(new ForumEntry("test 2","author2"));
+		testList.add(new ForumEntry("test 1","author1"));
 		
 		assertEquals(posts,testList);
 		
@@ -66,10 +70,9 @@ public class SortTest extends ActivityInstrumentationTestCase2<BrowseActivity> {
 	
 	public void sortByRatingTest(){
 		ArrayList<ForumEntry> posts = new ArrayList<ForumEntry>();
-		long time=123456789;
-		posts.add(new ForumEntry("test 1","author1",new Date(time)));
-		posts.add(new ForumEntry("test 2","author2",new Date(time)));
-		posts.add(new ForumEntry("test 3","author3",new Date(time)));
+		posts.add(new ForumEntry("test 1","author1"));
+		posts.add(new ForumEntry("test 2","author2"));
+		posts.add(new ForumEntry("test 3","author3"));
 		
 		posts.get(0).setUpVote(1);
 		posts.get(1).setUpVote(5);
@@ -77,12 +80,12 @@ public class SortTest extends ActivityInstrumentationTestCase2<BrowseActivity> {
 		
 		(new DataManager()).save(posts);
 		
-		testActivity.getBrowseControllerForTesting().sortByTime();
+		(new BrowseController()).sortByRating();
 		
 		ForumEntryList testList= new ForumEntryList();
-		testList.add(new ForumEntry("test 2","author2",new Date(time)));
-		testList.add(new ForumEntry("test 3","author3",new Date(time)));
-		testList.add(new ForumEntry("test 1","author1",new Date(time)));
+		testList.add(new ForumEntry("test 2","author2"));
+		testList.add(new ForumEntry("test 3","author3"));
+		testList.add(new ForumEntry("test 1","author1"));
 		
 		assertEquals(posts,testList);
 		
@@ -90,10 +93,9 @@ public class SortTest extends ActivityInstrumentationTestCase2<BrowseActivity> {
 	
 	public void sortByHasPictureTest(){
 		ArrayList<ForumEntry> posts = new ArrayList<ForumEntry>();
-		long time=123456789;
-		posts.add(new ForumEntry("test 1","author1",new Date(time)));
-		posts.add(new ForumEntry("test 2","author2",new Date(time)));
-		posts.add(new ForumEntry("test 3","author3",new Date(time)));
+		posts.add(new ForumEntry("test 1","author1"));
+		posts.add(new ForumEntry("test 2","author2"));
+		posts.add(new ForumEntry("test 3","author3"));
 		
 		posts.get(1).addPicture(new Picture());
 		
@@ -101,16 +103,16 @@ public class SortTest extends ActivityInstrumentationTestCase2<BrowseActivity> {
 		
 		(new DataManager()).save(posts);
 		
-		testActivity.getBrowseControllerForTesting().sortByTime();
+		(new BrowseController()).soryByHasPicture();
 		
 		ForumEntryList testList= new ForumEntryList();
-		testList.add(new ForumEntry("test 2","author2",new Date(time)));
-		testList.add(new ForumEntry("test 1","author1",new Date(time)));
-		testList.add(new ForumEntry("test 3","author3",new Date(time)));
+		testList.add(new ForumEntry("test 2","author2"));
+		testList.add(new ForumEntry("test 1","author1"));
+		testList.add(new ForumEntry("test 3","author3"));
 		
 		assertEquals(posts,testList);
 		
 		}
-	}
+}
 
 
