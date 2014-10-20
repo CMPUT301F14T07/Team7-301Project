3d2cb615b1a0c5a6e49ff15dd1773b34a73f52aa
DaynaLacoursiere <dlacours@ualberta.ca>
2014-10-20 15:54:43 -0600

    beautiful things are happening - main screen can now be used to access MyQuestions, ReadLaters, and AskScreen

--- F14T07-Application/gen/ca/ualberta/cs/f14t07_application/R.java
+++ F14T07-Application/gen/ca/ualberta/cs/f14t07_application/R.java
@@ -38,8 +38,8 @@ public final class R {
         public static final int browseButton=0x7f080009;
         public static final int invisButton=0x7f080007;
         public static final int logo=0x7f08000b;
-        public static final int myQuestionsDeleteCM=0x7f080015;
-        public static final int myQuestionsEditCM=0x7f080014;
+        public static final int myQuestionsDeleteCM=0x7f080016;
+        public static final int myQuestionsEditCM=0x7f080015;
         public static final int myQuestionsListView=0x7f08000e;
         public static final int myQuestionsTitle=0x7f08000d;
         public static final int name=0x7f080005;
@@ -53,9 +53,9 @@ public final class R {
         public static final int searchText=0x7f080008;
         public static final int signInButton=0x7f080006;
         public static final int subject=0x7f080004;
-        public static final int switchToHome=0x7f080012;
-        public static final int switchToMyQuestions=0x7f080016;
-        public static final int switchToReadLater=0x7f080013;
+        public static final int switchToHome=0x7f080014;
+        public static final int switchToMyQuestions=0x7f080013;
+        public static final int switchToReadLater=0x7f080012;
     }
     public static final class layout {
         public static final int activity_logo=0x7f030000;
@@ -67,23 +67,25 @@ public final class R {
     }
     public static final class menu {
         public static final int logo=0x7f070000;
-        public static final int my_questions=0x7f070001;
-        public static final int myquestionscontextmenu=0x7f070002;
-        public static final int read_later=0x7f070003;
-        public static final int readlatercontextmenu=0x7f070004;
+        public static final int main_menu=0x7f070001;
+        public static final int my_questions=0x7f070002;
+        public static final int myquestionscontextmenu=0x7f070003;
+        public static final int read_later=0x7f070004;
+        public static final int readlatercontextmenu=0x7f070005;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int ask=0x7f050004;
-        public static final int browse=0x7f050006;
-        public static final int hello_world=0x7f05000a;
-        public static final int logo=0x7f050008;
-        public static final int search=0x7f050005;
-        public static final int sign_in=0x7f050007;
+        public static final int ask=0x7f050005;
+        public static final int browse=0x7f050007;
+        public static final int hello_world=0x7f05000b;
+        public static final int logo=0x7f050009;
+        public static final int search=0x7f050006;
+        public static final int sign_in=0x7f050008;
+        public static final int title_activity_ask_screen=0x7f050004;
         public static final int title_activity_main_screen=0x7f050003;
         public static final int title_activity_my_questions=0x7f050002;
-        public static final int title_activity_read_later=0x7f050009;
+        public static final int title_activity_read_later=0x7f05000a;
     }
     public static final class style {
         /** 
--- F14T07-Application/src/ca/ualberta/cs/f14t07_application/MainScreenActivity.java
+++ F14T07-Application/src/ca/ualberta/cs/f14t07_application/MainScreenActivity.java
@@ -3,6 +3,8 @@ package ca.ualberta.cs.f14t07_application;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -25,6 +27,33 @@ public class MainScreenActivity extends Activity {
 		         }
 		    });
 	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    // option menu to move to different activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id) {
+        case R.id.switchToMyQuestions:
+        	Intent myQsIntent = new Intent(this, MyQuestionsActivity.class);
+        	startActivity(myQsIntent);
+        	return true;
+        case R.id.switchToReadLater:
+        	Intent readLaterIntent = new Intent(this, ReadLaterActivity.class);
+        	startActivity(readLaterIntent);
+        	return true;
+        default:
+        	return super.onOptionsItemSelected(item);
+        }
+    }
 	
 	
 	public void signInButton(){
