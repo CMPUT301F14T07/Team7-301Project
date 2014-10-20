f14b2fe55d4a1dce11378971d1e54fe41ce075cf
DaynaLacoursiere <dlacours@ualberta.ca>
2014-10-20 15:39:56 -0600

    working through some ugly kinks

--- F14T07-Application/gen/ca/ualberta/cs/f14t07_application/R.java
+++ F14T07-Application/gen/ca/ualberta/cs/f14t07_application/R.java
@@ -75,14 +75,15 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int ask=0x7f050003;
-        public static final int browse=0x7f050005;
-        public static final int hello_world=0x7f050009;
-        public static final int logo=0x7f050007;
-        public static final int search=0x7f050004;
-        public static final int sign_in=0x7f050006;
+        public static final int ask=0x7f050004;
+        public static final int browse=0x7f050006;
+        public static final int hello_world=0x7f05000a;
+        public static final int logo=0x7f050008;
+        public static final int search=0x7f050005;
+        public static final int sign_in=0x7f050007;
+        public static final int title_activity_main_screen=0x7f050003;
         public static final int title_activity_my_questions=0x7f050002;
-        public static final int title_activity_read_later=0x7f050008;
+        public static final int title_activity_read_later=0x7f050009;
     }
     public static final class style {
         /** 
