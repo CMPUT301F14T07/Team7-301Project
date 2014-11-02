f9e204989487f9969319684dfc045865245e2901
lexie bartlett <lexie@ualberta.ca>
2014-11-02 12:33:37 -0600

    Worked on ElasticSearch bug in code, not sure how to fix it :)

--- F14T07-Application/src/ca/ualberta/cs/f14t07_application/BrowseController.java
+++ F14T07-Application/src/ca/ualberta/cs/f14t07_application/BrowseController.java
@@ -1,10 +1,29 @@
 package ca.ualberta.cs.f14t07_application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class BrowseController {
-
+	private List<ForumEntry> forumEntries;
+	private SearchController searchController;
+	
 	public void setSessionAuthor(String author) {
 		MainScreenModel msm = new MainScreenModel();
 		msm.setSessionAuthor(author);
+	}
+	
+	public BrowseController(){ 
+		forumEntries = new ArrayList<ForumEntry>();
+		searchController = new SearchController();
+		
+	}
+	public List<ForumEntry> getAllEntries(){
+		forumEntries.addAll(searchController.searchAll());
+		ForumEntry forumEntry = new ForumEntry("browse","browse","browse");
+		forumEntries.add(forumEntry);
+		return forumEntries;
+		
 	}
 }
