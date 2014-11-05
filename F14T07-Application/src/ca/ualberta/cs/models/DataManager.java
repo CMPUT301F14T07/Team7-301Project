0ded1486ed9822dda15501ecd563c4dd364e0b72
lexie bartlett <lexie@ualberta.ca>
2014-11-05 16:37:00 -0600

    FIxed Ask Tests (forgot to commit a file in the last one)

--- F14T07-Application/src/ca/ualberta/cs/models/DataManager.java
+++ F14T07-Application/src/ca/ualberta/cs/models/DataManager.java
@@ -74,40 +74,6 @@ public class DataManager extends Activity {
 	public ForumEntry getForumEntry(){ 
 		return forumEntryTest;
 	}
-	//private Context context;
-	/*
-	public DataManager(Context appContext) {
-		context = appContext;
-	}
-
-		public ForumEntryList load() {
-			ForumEntryList forumEntryList = new ForumEntryList();
-			try {
-				FileInputStream fis = context.openFileInput(FILENAME);
-				ObjectInputStream ois = new ObjectInputStream(fis);
-				forumEntryList = (ForumEntryList) ois.readObject();
-				fis.close();
-				ois.close();
-			} catch (Exception e) {
-				Log.i("Kibbles", "Error casting");
-				e.printStackTrace();
-			} 
-			return forumEntryList;
-		}
-		
-		public void save(ForumEntryList forumEntryList) {
-			try {
-				FileOutputStream fos = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
-				ObjectOutputStream oos = new ObjectOutputStream(fos);
-				oos.writeObject(forumEntryList);
-				fos.close();
-				oos.close();
-			} 
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-		}*/ 
-
 	public void saveAuthor(String author) {
 		// TODO Auto-generated method stub
 		
