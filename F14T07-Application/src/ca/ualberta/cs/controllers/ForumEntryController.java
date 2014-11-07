9b7bd71d95c52442b9c55be3992aa5f1b8c71620
Brendan Bruner <bbruner@ualberta.ca>
2014-11-06 22:07:11 -0700

    Started documenting empty classes

--- F14T07-Application/src/ca/ualberta/cs/controllers/ForumEntryController.java
+++ F14T07-Application/src/ca/ualberta/cs/controllers/ForumEntryController.java
@@ -1,12 +1,32 @@
 package ca.ualberta.cs.controllers;
 
+import ca.ualberta.cs.models.DataManager;
 import ca.ualberta.cs.models.ForumEntry;
+import ca.ualberta.cs.models.ForumEntryList;
+import ca.ualberta.cs.views.Observer;
 
+/**
+ * Controls the data in the model of the QuestionActivity and EntryActivity. Also
+ * Controls the DataManager model and therefore what gets saved and loaded from memory
+ * and the remote server.
+ */
 public class ForumEntryController {
 
-	void addForumEntry(ForumEntry forumEntry){}
+	private ForumEntryList forumEntries;
+	private DataManager dataManager;
+	
+	/**
+	 * Creates a new ForumEntryController.
+	 * @param viewsContext The context of the view which aggregates the ForumEntryController.
+	 */
+	void ForumEntryController(Observer viewsContext){}
 
-	public void addQuestion(ForumEntry question) {
+	/**
+	 * Creates a new ForumEntry with the Question and adds it to the remote server, my authored,
+	 * and read later save locations
+	 * @param question The question which is being created
+	 */
+	public void addQuestion(Question question) {
 		// TODO Auto-generated method stub
 		
 	}
