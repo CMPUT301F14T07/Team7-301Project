5e1feb16f552eb6c47a1801c216fd7cb0ef085df
Brendan Bruner <bbruner@ualberta.ca>
2014-11-07 00:10:12 -0700

    Finished creation and documentation of singleton classes

--- F14T07-Application/src/ca/ualberta/cs/intent_singletons/ForumEntrySingleton.java
+++ F14T07-Application/src/ca/ualberta/cs/intent_singletons/ForumEntrySingleton.java
@@ -1,5 +1,48 @@
 package ca.ualberta.cs.intent_singletons;
 
-public class ForumEntrySingleton {
+import ca.ualberta.cs.models.ForumEntry;
 
+/**
+ * This is a singleton class that is useful for starting the QuestionActivity. It allows
+ * one activity to pass an instance of a ForumEntry around.
+ *
+ */
+public class ForumEntrySingleton 
+{
+	private static final ForumEntrySingleton forumEntrySingleton = new ForumEntrySingleton();
+	private ForumEntry forumEntry;
+	
+	private ForumEntrySingleton()
+	{
+		super();
+		this.forumEntry = null;
+	}
+	
+	/**
+	 * Get an instance of this singleton.
+	 * @return ForumEntrySingleton
+	 */
+	public static ForumEntrySingleton getInstance()
+	{
+		return forumEntrySingleton;
+	}
+	
+	/**
+	 * Set the ForumEntry in this singleton.
+	 * @param forumEntry
+	 */
+	public void setForumEntry(ForumEntry forumEntry)
+	{
+		this.forumEntry = forumEntry;
+	}
+	
+	
+	/**
+	 * Get the ForumEntry in this singleton.
+	 * @return ForumEntry
+	 */
+	public ForumEntry getForumEntry()
+	{
+		return this.forumEntry;
+	}
 }
