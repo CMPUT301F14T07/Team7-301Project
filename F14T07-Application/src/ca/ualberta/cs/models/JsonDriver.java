47293c699a69b0ef174137cc5d7e48615830ddae
mpietrasik <pietrasi@ualberta.ca>
2014-11-06 20:37:09 -0700

    Forgot to add JsonDriver class

---
+++ F14T07-Application/src/ca/ualberta/cs/models/JsonDriver.java
@@ -0,0 +1,33 @@
+package ca.ualberta.cs.models;
+
+import java.util.ArrayList;
+
+public class JsonDriver {
+	
+	public boolean setRemote(ArrayList<ForumEntry> fel){
+		
+		return false;
+	}
+	
+	public boolean setReomteBlyd(int x, ForumEntry fe){
+		
+		return false;
+	}
+
+	public boolean appendToRemote(ForumEntry fe){
+		
+		return false;
+	}
+
+	public ArrayList<ForumEntry> getRemote(String a, String b, String c){
+		
+		ArrayList<ForumEntry> fel=null;
+		return fel;
+	}
+	
+	public ForumEntry getRemoteBlyd(int x){
+		
+		ForumEntry fe=null;
+		return fe;
+	}
+}
