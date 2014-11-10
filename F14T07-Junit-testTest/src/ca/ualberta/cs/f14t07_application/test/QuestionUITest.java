2aa9f5c166fbe8188e90f07b848565134d4d7daa
lexie bartlett <lexie@ualberta.ca>
2014-11-10 15:47:57 -0700

    screw this

---
+++ F14T07-Junit-testTest/src/ca/ualberta/cs/f14t07_application/test/QuestionUITest.java
@@ -0,0 +1,64 @@
+package ca.ualberta.cs.f14t07_application.test;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.ListView;
+import ca.ualberta.cs.f14t07_application.R;
+import ca.ualberta.cs.models.Answer;
+import ca.ualberta.cs.models.DataManager;
+import ca.ualberta.cs.models.ForumEntry;
+import ca.ualberta.cs.models.Reply;
+import ca.ualberta.cs.views.QuestionActivity;
+
+public class QuestionUITest extends ActivityInstrumentationTestCase2<QuestionActivity> {
+	private QuestionActivity testActivity;
+	private Button testPostButton;
+	private Button testMainMenuButton;
+	private DataManager dm;
+	private Context context;
+	private ForumEntry fm;
+	public QuestionUITest()
+	{
+		super(QuestionActivity.class);
+	}
+	
+	@Override
+	protected void setUp() throws Exception
+	{
+	super.setUp();
+	setActivityInitialTouchMode(true);
+	testActivity = getActivity();
+	dm = new DataManager(context);
+	fm = new ForumEntry("subject","What is life?", "Kibbles");
+	dm.addForumEntry(fm);
+	}
+	
+	public void testViewReplies(){ 
+		Reply r = new Reply ("I hate eclipse");
+		fm.getQuestion().addReplies(r);
+		
+		ListView tv = (ListView) testActivity.findViewById(R.id.QuestionReplyList);
+		for(int i = 0; i<fm.getQuestion().getReplies().size();i++){
+			if (r == fm.getQuestion().getReplies().get(i)){
+				assert(false);
+			}
+		}
+	}
+
+	public void testViewAnswer(){ 
+		Answer r = new Answer ("I hate eclipse","kill me");
+		ArrayList<Answer>a = new ArrayList<Answer>();
+		a.add(r);
+		fm.setAnswer(a);
+		
+		ListView tv = (ListView) testActivity.findViewById(R.id.QuestionAnswerList);
+		for(int i = 0; i<fm.getAnswers().size();i++){
+			if (r == fm.getAnswers().get(i)){
+				assert(false);
+			}
+		}
+	}
+}
