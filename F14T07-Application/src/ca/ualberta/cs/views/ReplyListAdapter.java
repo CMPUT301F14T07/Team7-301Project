d51ef990e3b533d32e70904658da5ebd21cd3a2b
jfryan7 <jfryan@ualberta.ca>
2014-11-30 23:15:32 -0700

    reply list adaptor should now work

--- F14T07-Application/src/ca/ualberta/cs/views/ReplyListAdapter.java
+++ F14T07-Application/src/ca/ualberta/cs/views/ReplyListAdapter.java
@@ -3,7 +3,7 @@ package ca.ualberta.cs.views;
 import java.util.ArrayList;
 
 import ca.ualberta.cs.f14t07_application.R;
-import ca.ualberta.cs.models.Answer;
+import ca.ualberta.cs.models.Reply;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,44 +14,39 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 /**
- * This code is to be used for a making a list within a list on the question page (There is currently a list for 
- * answers but that list will be composed of upvote buttons, answers, and their respective replies). It is being left
- * empty for the moment because it will depend on how Entry.java, Question.java, Answer.java, ForumEntry.java and
- * ForumEntryList.java work, so until they are a little more filled out I will leave this code blank
+ *This is used for creating a list of different objects. More specifically for the replies list, includes the reply texts,
+ *authors and dates.
  * @author jfryan*/
-public class ReplyListAdapter extends ArrayAdapter<Answer>{
+public class ReplyListAdapter extends ArrayAdapter<Reply>{
 
 	private Context adaptersContext;
-	private ArrayList<Answer> AnswerList;
+	private ArrayList<Reply> ReplyList;
 	
-	public ReplyListAdapter(Context context, ArrayList<Answer> AnswerList)
+	public ReplyListAdapter(Context context, ArrayList<Reply> ReplyList)
 	{
-		super(context, android.R.layout.simple_list_item_1, android.R.id.text1, AnswerList);
+		super(context, android.R.layout.simple_list_item_1, android.R.id.text1, ReplyList);
 		this.adaptersContext = context;
-		this.AnswerList = AnswerList;
+		this.ReplyList = ReplyList;
 	}
 
-	/*
+	
 	
 	@Override
 	public View getView(int listPosition, View LinearLayout, ViewGroup parent)
 	{		
-		class TempAnswerList{
-			public Answer answer;
-			public TextView answerText;
+		class TempReplyList{
+			public Reply reply;
+			public TextView replyText;
 			public TextView author;
 			public TextView date;
-			public Button upvoteButton;
-			public TextView upvoteNumber;
-//			public ListView Replies;
 		};
-		final TempAnswerList temp = new TempAnswerList();
+		final TempReplyList temp = new TempReplyList();
 		
 		LayoutInflater inflater = (LayoutInflater) this.adaptersContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View listViewRow = inflater.inflate(R.layout.answer_list, parent, false);
+		View listViewRow = inflater.inflate(R.layout.reply_list, parent, false);
 		
-		temp.answer = this.AnswerList.get(listPosition);
-		temp.answerText = (TextView) listViewRow.findViewById(R.id.AnswerText);
+		temp.reply = this.ReplyList.get(listPosition);
+		temp.replyText = (TextView) listViewRow.findViewById(R.id.ReplyText);
 		
 		//temp.textView.setText(this.AnswerList.get(listPosition).getName());
 		
@@ -64,5 +59,5 @@ public class ReplyListAdapter extends ArrayAdapter<Answer>{
 		
 		return listViewRow;
 	
-}*/
+}
 }
